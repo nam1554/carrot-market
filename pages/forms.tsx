3a1157631191561f2509424de71d9efa2d59d1fd
GISUB NAM <gisubsub@gmail.com>
2022-11-16 23:19:26 +0900

    7.5 Errors

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -1,30 +1,55 @@
-import { useForm } from "react-hook-form";
+import { FieldErrors, useForm } from "react-hook-form";
 
-// Better validation
-// Better Erros (set, clear, display)
+// Better Erros (set, clear)
 // Have control over inputs
 
+interface LoginForm {
+  username: string;
+  password: string;
+  email: string;
+}
+
 export default function Forms() {
-  const { register, handleSubmit } = useForm();
-  const onValid = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginForm>({
+    mode: "onChange",
+  });
+  const onValid = (data: LoginForm) => {
     console.log("im valid bby");
   };
+  const onInvalid = (errors: FieldErrors) => {
+    console.log(errors);
+  };
   return (
-    <form onSubmit={handleSubmit(onValid)}>
+    <form onSubmit={handleSubmit(onValid, onInvalid)}>
       <input
         {...register("username", {
-          required: true,
+          required: "Username is required",
+          minLength: {
+            message: "The username should be longer than 5 chars.",
+            value: 5,
+          },
         })}
         type="text"
         placeholder="Username"
       />
       <input
-        {...register("email", { required: true })}
+        {...register("email", {
+          required: "Email is required",
+          validate: {
+            notGmail: (value) =>
+              !value.includes("@gmail.com") || "Gmail is not allowed",
+          },
+        })}
         type="email"
         placeholder="Email"
       />
+      {errors.email?.message}
       <input
-        {...register("password", { required: true })}
+        {...register("password", { required: "Password is required" })}
         type="password"
         placeholder="Password"
       />
